ecae3946a4fe2073276ed6cab05a83f7bd3624c7
jokerseed <573202560@qq.com>
2019-08-02 18:40:33 +0800

    。

--- PersonalEgretProject/src/core/mvc/view/ViewManager.ts
+++
@@ -1,9 +0,0 @@
-namespace game {
-    export class ViewManager {
-        private static _instance: ViewManager;
-
-        public static get instance(): ViewManager {
-            return ViewManager._instance || (ViewManager._instance = new ViewManager());
-        }
-    }
-}
