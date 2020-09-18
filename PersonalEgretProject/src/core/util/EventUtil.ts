ebd35570d320226a956713b42c1a41f3b777e9f4
573202560@qq.com <LIUquan123>
2020-09-18 10:16:33 +0800

    。

---
+++ PersonalEgretProject/src/core/util/EventUtil.ts
@@ -0,0 +1,15 @@
+namespace game {
+
+
+    class EventUtil extends egret.EventDispatcher{
+        public dispatch(){
+            // this.dispatchEventWith();
+        }
+
+        public addListen(){
+            // this.addEventListener();
+        }
+    }
+
+    export const eventUtil = new EventUtil();
+}
